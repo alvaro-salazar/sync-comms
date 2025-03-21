6594847add4602351c3742fb6a675806aa000796
Alvaro Salazar <ah.salazar@uniandes.edu.co>
2025-03-20 19:19:29 -0500

    feat: proyecto de comunicaciones sincronicas inicializado

---
+++ producto-service-demo/src/main/java/com/example/productoservicedemo/ProductoServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.productoservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductoServiceDemoApplication.class, args);
+    }
+
+}
